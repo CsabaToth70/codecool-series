932db014d552166f39960528b5be720a4feb3a11
CsabaToth70 <lesitocsa@gmail.com>
2020-12-10 13:23:56 +0100

    add conditional background color to rows

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,14 +1,17 @@
-// function getBackground (){
-    // let episodeValues = document.querySelectorAll(".episodes").textContent;
-    // console.log('episodeValues');
-    // for (let episodeValue of episodeValues){
-    //     if (parseInt(episodeValue) % 2 === 0){
-    //         console.log('even');
-    //         episodeValue.closest('row').classList.add('even')
-    //     } else {
-    //         console.log('odd');
-    //         episodeValue.closest('row').classList.add('odd')
-    //     }
-    //
-    // }
-// }
+function init(){
+    getBackground();
+}
+
+function getBackground (){
+    let episodeValues = document.querySelectorAll(".episodes");
+    for (let episodeValue of episodeValues){
+        console.log(episodeValue.textContent);
+        if (Number(episodeValue.textContent) % 2 === 0){
+            episodeValue.parentElement.classList.add('even')
+        } else {
+            episodeValue.parentElement.classList.add('odd')
+        }
+    }
+}
+
+init()
